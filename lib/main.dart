9533f69355a6ca00861da25e442607dfa3c51bfd
Yhan Nunes <51480561+Yhan17@users.noreply.github.com>
2021-09-09 10:30:06 -0300

    update to null safety

--- lib/main.dart
+++ lib/main.dart
@@ -18,48 +18,52 @@ class _HomeState extends State<Home> {
   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   String _infoText = "Informe seus dados";
-  void _resetFields(){
+  void _resetFields() {
     weightController.text = "";
     heightController.text = "";
     setState(() {
       _infoText = "Informe seus dados";
     });
-
   }
-  void _calculate(){
+
+  void _calculate() {
     setState(() {
-      double weight = double.parse(weightController.text);
-      double height = double.parse(heightController.text) / 100;
+      double weight = double.parse(weightController.text.replaceAll(",", "."));
+      double height =
+          double.parse(heightController.text.replaceAll(",", ".")) / 100;
       double imc = weight / (height * height);
-      if(imc < 18.6)
+      if (imc < 18.6)
         _infoText = "Abaixo do Peso Imc= (${imc.toStringAsPrecision(4)})";
-      else if(imc >= 18.6 && imc < 4.9)
+      else if (imc >= 18.6 && imc < 4.9)
         _infoText = "Peso ideal Imc= (${imc.toStringAsPrecision(4)})";
-      else if(imc >= 24.9 && imc< 29.9)
-        _infoText = "Levemente Acima do Peso Imc= (${imc.toStringAsPrecision(4)})";
-      else if(imc >= 29.9 && imc < 34.9)
+      else if (imc >= 24.9 && imc < 29.9)
+        _infoText =
+            "Levemente Acima do Peso Imc= (${imc.toStringAsPrecision(4)})";
+      else if (imc >= 29.9 && imc < 34.9)
         _infoText = "Obesidade Grau I Imc= (${imc.toStringAsPrecision(4)})";
-      else if(imc >= 34.9 && imc < 39.9)
+      else if (imc >= 34.9 && imc < 39.9)
         _infoText = "Obesidade Grau II Imc= (${imc.toStringAsPrecision(4)})";
-      else if(imc >= 40)
+      else if (imc >= 40)
         _infoText = "Obesidade Grau III Imc= (${imc.toStringAsPrecision(4)})";
-
     });
-
   }
 
   @override
   Widget build(BuildContext context) {
     //Barra No topo Onde fica o título e a adição de um botão refresh
-    return Scaffold(
+    return GestureDetector(
+      onTap: FocusScope.of(context).unfocus,
+      child: Scaffold(
         appBar: AppBar(
           title: Text("Calculadora de IMC"),
           centerTitle: true,
           backgroundColor: Colors.green,
           actions: <Widget>[
-            IconButton(icon: Icon(Icons.refresh), onPressed: () {
-              _resetFields();
-            })
+            IconButton(
+                icon: Icon(Icons.refresh),
+                onPressed: () {
+                  _resetFields();
+                })
           ],
         ),
         backgroundColor: Colors.white,
@@ -78,43 +82,47 @@ class _HomeState extends State<Home> {
                 TextFormField(
                   keyboardType: TextInputType.number,
                   decoration: InputDecoration(
-                      labelText: "Peso (kg):",
-                      labelStyle: TextStyle(color: Colors.green)),
+                    labelText: "Peso (kg):",
+                    labelStyle: TextStyle(color: Colors.green),
+                  ),
                   textAlign: TextAlign.center,
                   style: TextStyle(color: Colors.green, fontSize: 25),
                   controller: weightController,
-                  validator: (value){
-                    if(value.isEmpty) {
+                  validator: (value) {
+                    if (value!.isEmpty) {
                       return "Insira seu peso";
                     }
                   },
                 ),
                 TextFormField(
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                        labelText: "Altura (cm):",
-                        labelStyle: TextStyle(color: Colors.green)),
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: Colors.green, fontSize: 25),
-                    controller: heightController,
-                validator: (value) {
-                  if(value.isEmpty) {
-                    return "Insira seu peso";
-                  }
-                }
-          ),
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: "Altura (cm):",
+                    labelStyle: TextStyle(color: Colors.green),
+                  ),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.green, fontSize: 25),
+                  controller: heightController,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Insira seu peso";
+                    }
+                  },
+                ),
                 Padding(
                   padding: EdgeInsets.only(top: 10, bottom: 10),
                   child: Container(
                     height: 50,
-                    child: RaisedButton(
+                    child: ElevatedButton(
                       onPressed: () {
-                        if(_formKey.currentState.validate())
-                        _calculate();
+                        if (_formKey.currentState!.validate()) _calculate();
                       },
                       child: Text("Calcular",
                           style: TextStyle(color: Colors.white, fontSize: 25)),
-                      color: Colors.green,
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.green),
+                      ),
                     ),
                   ),
                 ),
@@ -125,7 +133,9 @@ class _HomeState extends State<Home> {
                 )
               ],
             ),
-          )
-        ));
+          ),
+        ),
+      ),
+    );
   }
 }
